78bc841a185183f3df02d34ce3b205e47ab4784f
Horolaker <horolaker@yandex.ru>
2023-08-22 23:20:51 +0300

    feat style

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,12 +1,14 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 import java.time.LocalDate;
-import java.util.Collection;
+import java.util.List;
+import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,24 +16,19 @@ class FilmControllerTest {
     private FilmController filmController;
 
     @BeforeEach
-    public void setUp() {
-        filmController = new FilmController();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        filmController = null;
+    public void init() {
+        filmController = new FilmController(new FilmService(new InMemoryFilmStorage()));
     }
 
     @Test
-    void findAll() { // Тест вывода списка Films
-        Collection<Film> allFilmsCreated = filmController.findAll();
+    void findAll() {
+        List<Film> allFilmsCreated = filmController.findAll();
         assertNotNull(allFilmsCreated);
     }
 
     @Test
-    void add() { //Тест добавления Film
-        Film createdFilm = filmController.add(new Film(1, "FilmName", "Description", LocalDate.of(1990, 11, 11), 2));
+    void add() {
+        Film createdFilm = filmController.add(new Film(1, "FilmName", "Description", LocalDate.of(1990, 11, 11), 2, new TreeSet<>()));
         assertNotNull(createdFilm.getId());
         assertNotNull(createdFilm.getName());
         assertNotNull(createdFilm.getDescription());
@@ -39,9 +36,9 @@ class FilmControllerTest {
     }
 
     @Test
-    void update() { // Тест обновления Film
-        Film createdFilm = filmController.add(new Film(1, "FilmName", "Description", LocalDate.of(1990, 11, 11), 2));
-        Film updatedFilm = new Film(1, "FilmNameUpdated", "Description", LocalDate.of(1990, 11, 11), 2);
+    void update() {
+        Film createdFilm = filmController.add(new Film(1, "FilmName", "Description", LocalDate.of(1990, 11, 11), 2, new TreeSet<>()));
+        Film updatedFilm = new Film(1, "FilmNameUpdated", "Description", LocalDate.of(1990, 11, 11), 2, new TreeSet<>());
         filmController.update(updatedFilm);
         assertNotNull(createdFilm.getId());
         assertNotNull(createdFilm.getName());
